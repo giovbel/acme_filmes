4795902e86bfb3c293bf3e1bc6a1452259aa1ef1
GIOVANNA SILVA BELO <23111786@CFP127ALUNO.LOCAL>
2024-03-21 09:38:07 -0300

    fazendo

--- js/filmes.js
+++ js/filmes.js
@@ -1,11 +1,40 @@
 export async function postFilme (Filme) {
-    const url = 'http://10.107.134.46:8080/v2/acmeFilmes/filme'
+
+    const url = 'http://10.107.134.17:8080/v2/acmeFilmes/filme'
     const options = {
-        method: 'POST'
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify (Filme)
+    }
+
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+export async function putFilme (Filme) {
+
+    const url = `http://10.107.134.17:8080/v2/acmeFilmes/atualizarFilme/${filme.id}`
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify (Filme)
+    }
+
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+export async function deleteFilme (id) {
+
+    const url = `http://10.107.134.17:8080/v2/acmeFilmes/atualizarFilme/${id}`
+    const options = {
+        method: 'DELETE'
     }
 
     const response = await fetch(url, options)
